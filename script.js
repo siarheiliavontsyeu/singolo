385ca8f135bc166da580c4c22c6c53e74b89e6cd
Siarhei Liavontsyeu <leontevsi@gmail.com>
2020-03-12 14:19:16 +0300

    feat: add sticky header

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 // navigation
+const HEADER = document.getElementById("header");
 const NAVIGATION = document.getElementById('navigation');
 
+/* sticky header*/
+window.onscroll = function() {
+    if (window.pageYOffset > 0) {
+        HEADER.classList.add('header_sticky');
+    } else {
+        HEADER.classList.remove('header_sticky');
+    }
+};
 
+/* navigation */
 NAVIGATION.addEventListener('click', (event) => {
+    console.log(event);
     event.preventDefault();
-
     // add/remove active class
     NAVIGATION.querySelectorAll('a').forEach(elem => {
         elem.classList.remove('navigation__link_active');
     });
-    event.target.classList.add('navigation__link_active');
+    event.target.tagName === 'A' && event.target.classList.add('navigation__link_active');
 
     //smooth scroll
-    const anchorID = event.target.getAttribute('href').substr(1);
+    const anchorID = event.target.tagName === 'A' && event.target.getAttribute('href').substr(1);
 
-    document.getElementById(anchorID).scrollIntoView({
+    event.target.tagName === 'A' && document.getElementById(anchorID).scrollIntoView({
         behavior: 'smooth',
-        block: 'start'
+        block: 'center'
     });
 });
